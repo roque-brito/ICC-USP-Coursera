628383080fd44606c7ab1927b3dc3062b47c0c88
roque-brito <roque.brito@outlook.com>
2021-06-10 21:37:14 -0300

    semana6 ex02 okay

--- icc_pt2/week6/Exercicios/exe02_encontra_impares_numa_lista.py
+++ icc_pt2/week6/Exercicios/exe02_encontra_impares_numa_lista.py
@@ -1,22 +1,40 @@
 
 def encontra_impares(lista):
     ''' lista(inteiros) -> lista(inteiros ímpares) '''
-    lista_impar = []
-    if ( lista[0] % 2 ) != 0:
-        lista_impar.extend([lista[0]])
-        return lista_impar
-    else:
-        lista.remove(lista[0])
-        encontra_impares(lista)
+    
+    # Criar uma lista vazia p/ adicionar os nº ímpares:
+    lista_impares = []
 
+    # Verificar se a lista está vazia:
+    if len(lista) > 0:
+        
+        # Verifica se o numero é par:
+        if lista[0] % 2 == 0:
+            
+            # Remove o nº par da lista
+            lista.remove(lista[0])
 
-    return lista_impar
+            # Chamada recursiva com a lista sem o número par: 
+            return  encontra_impares(lista)    
+        
+        else:
+            # Adiciona o elemeto ímpar a lista vazia:
+            lista_impares.extend([lista[0]])
 
+            # Cria uma nova lista excluindo o primeiro elemento:
+            lista = lista[1:]
+            
+        # Atualiza a nova lista com ímpares:
+        lista_impares += encontra_impares(lista)
+            
+    return lista_impares
 
 
 # -------------- TESTE --------------
 if __name__ == '__main__':
     lista0 = [0, 1]     
-    lista1 = [1, 1, 2, 3, 11, 14, 5, 3, 20]
-    lista_inpares = encontra_impares(lista1)
-    print(lista_inpares)
+    lista1 = [1, 3, 2, 5, 4]
+    lista2 = [1, 1, 2, 3, 4, 11, 14, 5, 3, 20, 23]
+    
+    lista_impares = encontra_impares(lista2)
+    print(lista_impares)
